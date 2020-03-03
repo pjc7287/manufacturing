0c3339e3f4dbb25932aef7e4f62bc07c9687c1da
Kevin Adrian <kxa1335.g.rit.edu>
2020-03-02 23:54:31 -0500

    Filled in WorkOrder and WorkOrderTable classes

--- manufacturing/src/main/java/domain/WorkOrder.java
+++ manufacturing/src/main/java/domain/WorkOrder.java
@@ -1,8 +1,37 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WorkOrder {
 
-    private String type;
+    private int id;
+    private String action;
+    private int prod_id;
+    private String desc;
+
+    public WorkOrder(int id, String action, int prod_id, String desc){
+        this.id = id;
+        this.action = action;
+        this.prod_id = prod_id;
+        this.desc = desc;
+    }
+
+    public void setProd_id(int prod_id) { this.prod_id = prod_id; }
+
+    public int getProd_id() { return prod_id; }
+
+    public void setId(int id) { this.id = id; }
+
+    public int getId() { return id; }
+
+    public void setAction(String action) { this.action = action; }
+
+    public String getAction() { return action; }
+
+    public void setDesc(String desc) { this.desc = desc; }
+
+    public String getDesc() { return desc; }
 
 
 }
